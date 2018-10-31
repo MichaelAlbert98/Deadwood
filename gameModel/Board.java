a305f9d4860b4d02b6821c5710a5f3e337edae46
Michael Albert <albertclan@frontier.com>
2018-10-31 11:23:07 -0700

    Update Board.java

---
+++ gameModel/Board.java
@@ -0,0 +1,32 @@
+public class Board {
+
+   private Room[] roomList;
+   private map<String,Room> nameToRoom;
+   private Room[][] adjacentRooms;
+   private int scenesRemaining;
+   
+   public Board(Room[] roomList, map<String,Room> nameToRoom, Room[][] adjacentRooms) {
+   
+   }
+   
+   public nameToRoom(String name) {
+   
+   }
+   
+   public areRoomsAdjacent(Room start, Room end) {
+   
+   }
+   
+   public isValidMove(Room 1, Room 2) {
+   
+   }
+   
+   public Scene getScenesRemaining {
+   
+   }
+   
+   public void setScenesRemaining {
+   
+   }
+   
+}
