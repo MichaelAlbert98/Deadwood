f92b01432716703ff847359481df98a4d1a9253d
Michael Albert <albertclan@frontier.com>
2018-10-31 11:35:29 -0700

    Update Banker.java

---
+++ gameModel/Banker.java
@@ -0,0 +1,47 @@
+public class Banker {
+   
+   private int rank;
+   private int cash;
+   private int credits;
+   
+   public Banker(int rank, int cash, int credits) {
+   
+   }
+   
+   public void upgrade(moneyType,rank) {
+   
+   }
+   
+   public int getRank() {
+   
+   }
+   
+   public void setRank(int rank) {
+   
+   }
+   
+   public int getCash() {
+   
+   }
+   
+   public void setCash(int amount) {
+   
+   }
+   
+   public void addCash(int amount) {
+   
+   }
+   
+   public int getCredits() {
+   
+   }
+   
+   public void setCredits(int amount) {
+   
+   }
+   
+   public void addCredits(int amount) {
+   
+   }
+   
+}
