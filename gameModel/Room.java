1fe9690fc2cbbe9871681f8bfcac3ffee537da29
Michael Albert <albertclan@frontier.com>
2018-10-31 11:24:04 -0700

    Update Room.java

---
+++ gameModel/Room.java
@@ -0,0 +1,55 @@
+public class Room {
+
+   private String name;
+   private String[] adjacentRooms;
+   private int roomIndex;
+   private int counters;
+   private Player[] playersInRoom;
+   private Role[] roomRoles;
+   private Scene roomScene;
+   
+   public Room(String name, String[] adjRooms, int index) {
+   
+   }
+   
+   public String getRoomName() {
+   
+   }
+   
+   public String[] getAdjRooms() {
+   
+   }
+   
+   public int getRoomIndex() {
+   
+   }
+   
+   public int getCounters() {
+   
+   }
+   
+   public void setCounters() {
+   
+   }
+   
+   public Player[] getPlayersInRoom() {
+   
+   }
+   
+   public void setPlayersInRoom() {
+   
+   }
+   
+   public Role[] getRoles() {
+   
+   }
+   
+   public Scene getScene() {
+   
+   }
+   
+   public void setScene() {
+   
+   }  
+   
+}
