f93ebed5cf3245d40c2addd9057baa5a52f4c462
Michael Albert <albertclan@frontier.com>
2018-10-31 19:26:56 -0700

    Update Card.java

---
+++ gameModel/Card.java
@@ -0,0 +1,48 @@
+public class Card {
+
+   private String name;
+   private int budget;
+   private Room location;
+   private Role[] roles;
+   
+   public Card() {
+   
+   }
+   
+   public Card(String name, int budget, Room loc, Role[] roles) {
+   
+   }
+   
+   public String getName() {
+   
+   }
+   
+   public void setName(String name) {
+   
+   }
+   
+   public int getBudget() {
+   
+   }
+   
+   public void setBudget(int budget) {
+   
+   }
+   
+   public Room getLocation() {
+   
+   }
+   
+   public void setLoccation(Room loc) {
+   
+   }
+   
+   public Role[] getRoles() {
+   
+   }
+   
+   public void setRoles(Role[] roles) {
+   
+   }
+   
+}
