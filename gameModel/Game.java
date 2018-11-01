517bbfbf6b4a1516e6903ba22e8a8bbc9a3046e6
Michael Albert <albertclan@frontier.com>
2018-10-31 23:05:38 -0700

    Update Game.java

--- gameModel/Game.java
+++ gameModel/Game.java
@@ -8,47 +8,56 @@ public class Game {
    private int currentDay;
    
    public Game() {
-   
+      this.board = null;
+      this.deck = null;
+      this.playerList = null;
+      this.currentPlayer = null;
+      this.playerCount = 0;
+      this.currentDay = 1;
    }
    
    public Game(Board board, Deck deck, Player[] playerlist, int playerCount) {
-   
+      this.board = board;
+      this.deck = deck;
+      this.playerList = playerList;
+      this.playerCount = playerCount;
+      this.currentDay = 1;
    }
    
    private void startGame() {
-   
+      return;
    }
    
    private void newDay() {
-   
+      return;
    }
    
    private void upgradePhase() {
-   
+      return;
    }
    
    private void movementPhase() {
-   
+      return;
    }
    
    private void actingPhase() {
-   
+      return;
    }
    
    private void calculateScores() {
-   
+      return;
    }
    
    private void displayScores() {
-   
+      return;
    }
    
    private void declareWinner() {
-   
+      return;
    }
    
    private void endGame() {
-   
+      return;
    }
    
 }
