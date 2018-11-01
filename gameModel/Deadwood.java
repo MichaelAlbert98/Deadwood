aa0c35a4e7b6e58984afaa38633df122a207d315
rtlingg <linggr@wwu.edu>
2018-11-01 13:10:37 -0700

    Updated Deadwood.java and Game.java

--- gameModel/Deadwood.java
+++ gameModel/Deadwood.java
@@ -1,13 +1,78 @@
+/* CSCI 345 - Object Oriented Design - Deadwood
+ * Ryan Lingg and Michael Albert
+ * Deadwood.java
+ * 11/31/11
+ */
+
+
 public class Deadwood {
-   
-   public int playerCount;
-   
-   public static void main(String[] args) {
-   
-   }
-   
-   public int askPlayerCount() {
-      return 0;
-   }
-   
+
+    //Deadwood Attributes
+    private static int numPlayers;
+    private static final int minPlayers = 2;
+    private static final int maxPlayers = 8;
+    private static int numDays;
+    private static Game game;
+    
+
+    /* Main Method
+    * 
+    * Controller for the Deadwood game.
+    */
+    public static void main(String[] args) {
+
+        try {
+
+            //1) Set the number of players for the game
+            if (args.length == 0){
+                setNumPlayers = 2;
+            } else {
+                setNumPlayers(args[0]);
+            }
+
+            //2) Set the number of days designated for this player count
+            setNumDays();
+
+            //3) Initalize and start the game instance
+            Game g = new Game(numPlayers,numDays);
+
+        } catch (numPlayersException e) {
+            System.out.println("There must be between 2 and 8 players.");
+        } catch (NumberFormatException e){
+            System.out.println("Number of players desired must be a integer between 2 and 8.");
+        } catch (exception e){
+            System.out.println("Unidentified Error encountered. Exiting Deadwood.");
+        }
+
+        System.out.println("Game Over.");
+    }
+
+
+    /* setNumPlayers
+    *
+    * Setter for the number of players in a Deadwood game.
+    */
+    private setNumPlayers(String countStr) throws numPlayersException, NumberFormatException {
+        int playerCount;
+        if ((playerCount = Integer.parseInt(countStr))) {
+
+        }
+        if ((playerCount < minPlayers) && (playerCount > maxPlayers)) {
+            throw new numPlayersException();
+        } else {
+            numPlayers = playerCount;
+        }
+    }
+
+    /* setNumPlayers
+    *
+    * Setter for the number of players in a Deadwood game.
+    */
+    private setNumDays() throws numPlayersException, NumberFormatException {
+        if (numPlayers < 4) {
+            numDays = 3;
+        } else {
+            numDays = 4;
+        }
+    }
 }
--- gameModel/Game.java
+++ gameModel/Game.java
@@ -1,63 +1,26 @@
-public class Game {
-   
-   private Board board;
-   private Deck deck;
-   private Player[] playerList;
-   private Player currentPlayer;
-   private int playerCount;
-   private int currentDay;
-   
-   public Game() {
-      this.board = null;
-      this.deck = null;
-      this.playerList = null;
-      this.currentPlayer = null;
-      this.playerCount = 0;
-      this.currentDay = 1;
-   }
-   
-   public Game(Board board, Deck deck, Player[] playerlist, int playerCount) {
-      this.board = board;
-      this.deck = deck;
-      this.playerList = playerList;
-      this.playerCount = playerCount;
-      this.currentDay = 1;
-   }
-   
-   private void startGame() {
-      return;
-   }
-   
-   private void newDay() {
-      return;
-   }
-   
-   private void upgradePhase() {
-      return;
-   }
-   
-   private void movementPhase() {
-      return;
-   }
-   
-   private void actingPhase() {
-      return;
-   }
-   
-   private void calculateScores() {
-      return;
-   }
-   
-   private void displayScores() {
-      return;
-   }
-   
-   private void declareWinner() {
-      return;
-   }
-   
-   private void endGame() {
-      return;
-   }
-   
+/* CSCI 345 - Object Oriented Design - Deadwood
+ * Ryan Lingg and Michael Albert
+ * Deadwood.java
+ * 11/31/11
+ */
+
+
+public class Game extends Deadwood {
+
+    //Game Attributes
+    private Board board;
+    private Deck deck;
+    private ArrayList<Players> playerList;
+    private Player activePlayer;
+    private int currentDay;
+
+
+    public Game(int numPlayers, int numDays){
+
+    }
+
+    public newDay() {
+
+    }
+
 }
