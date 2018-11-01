17d906950d1b405b76072c3f5eac45f2edaddc41
Michael Albert <albertclan@frontier.com>
2018-10-31 23:06:07 -0700

    Update Room.java

--- gameModel/Room.java
+++ gameModel/Room.java
@@ -9,55 +9,69 @@ public class Room {
    private Scene roomScene;
    
    public Room() {
-   
+      this.name = "";
+      this.adjacentRooms = null;
+      this.roomIndex = 0;
+      this.counters = 0;
+      this.roomRoles = null;
+      this.roomScene = null;
    }
    
    public Room(String name, String[] adjRooms, int index, int counters, Role[] roomRoles, Scene roomScene) {
-   
+      this.name = name;
+      this.adjacentRooms = adjRooms;
+      this.roomIndex = index;
+      this.counters = counters;
+      this.roomRoles = roomRoles;
+      this.roomScene = roomScene;
    }
    
-   public String getRoomName() {
-   
+   public String getName() {
+      return this.name;
    }
    
    public String[] getAdjRooms() {
-   
+      return this.adjacentRooms;
    }
    
    public int getRoomIndex() {
-   
+      return this.roomIndex;
    }
    
    public int getCounters() {
-   
+      return this.counters;
    }
    
-   public void setCounters() {
-   
+   public void setCounters(int value) {
+      this.counters = value;
+      return;
    }
    
    public Player[] getPlayersInRoom() {
-   
+      return playersInRoom;
    }
    
-   public void setPlayersInRoom() {
-   
+   public void setPlayersInRoom(Player player) {
+      this.playersInRoom[0] = player;
+      return;
    }
    
    public Role[] getRoles() {
-   
+      return roomRoles;
    }
    
    public void setRoles(Role[] roles) {
-   
+      this.roomRoles = roles;
+      return;
    }
    
    public Scene getScene() {
-   
+      return roomScene;
    }
    
-   public void setScene() {
-   
+   public void setScene(Scene scene) {
+      this.roomScene = scene;
+      return;
    }  
    
 }
