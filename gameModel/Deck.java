41fcec54fa00d7bfe4951b7a5c596319251f6100
Michael Albert <albertclan@frontier.com>
2018-10-31 23:06:33 -0700

    Update Deck.java

--- gameModel/Deck.java
+++ gameModel/Deck.java
@@ -4,31 +4,33 @@ public class Deck {
    private int topIndex;
    
    public Deck() {
-   
+      this.cards = null;
+      this.topIndex = 0;
    }
    
    public Deck(Card[] cards) {
-   
+      this.cards = cards;
+      this.topIndex = 0;
    }
    
    private void shuffleDeck() {
-   
+      return;
    }
    
    public Card[] getCards() {
-   
+      return this.cards;
    }
    
    public void setCards(Card[] cards) {
-   
+      this.cards = cards;
    }
    
    public int getTopIndex() {
-   
+      return this.topIndex;
    }
    
    public void setTopIndex(int value) {
-   
+      this.topIndex = value;
    }
    
 }
