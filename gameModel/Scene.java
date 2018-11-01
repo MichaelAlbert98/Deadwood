eb6d12363aef38d88f1aef549520b1b07ab7bf6c
Michael Albert <albertclan@frontier.com>
2018-10-31 19:11:31 -0700

    Update Scene.java

---
+++ gameModel/Scene.java
@@ -0,0 +1,70 @@
+public class Scene{
+   
+   private Boolean isCardUp;
+   private Boolean isSceneDone;
+   private Card sceneCard;
+   private Role[] sceneRoles;
+   private int shotsLeft;
+   private int budget;
+   
+   public Scene() {
+   
+   }
+   
+   public Scene(Card card, Role[] roles, int shots, int budget) {
+   
+   }
+   
+   private void wrapScene() {
+   
+   }
+   
+   public Boolean getIsCardUp() {
+   
+   }
+   
+   public void setIsCardUp(Boolean truth) {
+   
+   }
+   
+   public Boolean getIsSceneDone() {
+   
+   }
+   
+   public void setIsSceneDone(Boolean truth) {
+   
+   }
+   
+   public Card getSceneCard() {
+   
+   }
+   
+   public void setSceneCard(Card card) {
+   
+   }
+   
+   public Role[] getSceneRoles() {
+   
+   }
+   
+   public void setSceneRoles(Role[] roles) {
+   
+   }
+   
+   public int getShotsLeft() {
+   
+   }
+   
+   public void setShotsLeft(int value) {
+   
+   }
+   
+   public int getBudget() {
+   
+   }
+   
+   public void setBudget(int budget) {
+   
+   }
+  
+}
