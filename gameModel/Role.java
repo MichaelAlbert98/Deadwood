8d0a47b0b3266ece31627515788a67eaf12a99e1
Michael Albert <albertclan@frontier.com>
2018-10-31 18:15:02 -0700

    Update Role.java

---
+++ gameModel/Role.java
@@ -0,0 +1,32 @@
+public class Role {
+   
+   private String name;
+   private Boolean isCardRole;
+   private Boolean isTaken;
+   private int requiredRank;
+   
+   public Role() {
+   
+   }
+   
+   public Role(String name, int rank) {
+   
+   }
+   
+   public Boolean getOnCard() {
+   
+   }
+   
+   public int getRank() {
+   
+   }
+   
+   public Boolean getIsTaken() {
+   
+   }
+   
+   public void setIsTaken() {
+   
+   }
+   
+}
