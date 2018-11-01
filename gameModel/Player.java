d3128b7f1e0c4a760399a6cc4af637d57d1d1185
Michael Albert <albertclan@frontier.com>
2018-10-31 18:07:53 -0700

    Update Player.java

---
+++ gameModel/Player.java
@@ -0,0 +1,64 @@
+public class Player {
+
+   private String name;
+   private Boolean hasRole;
+   private Role currentRole;
+   private int rehearseTokens;
+   
+   public Player() {
+   
+   }
+   
+   public Player(String name) {
+   
+   }
+   
+   public String promptPlayer(String prompt) {
+   
+   }
+   
+   public void shotSuccess(Role role) {
+   
+   }
+   
+   public void shotFailure(Role role) {
+   
+   }
+   
+   public String getName() {
+   
+   }
+   
+   public void setName(String name) {
+   
+   }
+   
+   public Boolean getHasRole() {
+   
+   }
+   
+   public void setHasRole(Boolean truth) {
+   
+   }
+   
+   public Role getCurrentRole() {
+   
+   }
+   
+   public void setCurrentRole(Role role) {
+   
+   }
+   
+   public void removeRole() {
+   
+   }
+      
+   public int getTokens() {
+   
+   }
+   
+   public void setTokens(int value) {
+   
+   }
+   
+}
