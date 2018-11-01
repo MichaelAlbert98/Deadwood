f6bb0d8b294709e99422877b0297965c665a0dfe
Michael Albert <albertclan@frontier.com>
2018-10-31 23:07:23 -0700

    Update Player.java

--- gameModel/Player.java
+++ gameModel/Player.java
@@ -7,59 +7,69 @@ public class Player {
    private Banker banker;
    
    public Player() {
-   
+      this.name = "";
+      this.hasRole = false;
+      this.currentRole = null;
+      this.rehearseTokens = 0;
    }
    
    public Player(String name) {
-   
+      this.name = name;
+      this.hasRole = false;
+      this.currentRole = null;
+      this.rehearseTokens = 0;
    }
    
    public String promptPlayer(String prompt) {
-   
+      return prompt;
    }
    
    public void shotSuccess(Role role) {
-   
+      return;
    }
    
    public void shotFailure(Role role) {
-   
+      return;
    }
    
    public String getName() {
-   
+      return this.name;
    }
    
    public void setName(String name) {
-   
+      this.name = name;
+      return;
    }
    
    public Boolean getHasRole() {
-   
+      return this.hasRole;
    }
    
    public void setHasRole(Boolean truth) {
-   
+      this.hasRole = truth;
+      return;
    }
    
    public Role getCurrentRole() {
-   
+      return this.currentRole;
    }
    
    public void setCurrentRole(Role role) {
-   
+      this.currentRole = role;
+      return;
    }
    
    public void removeRole() {
-   
+      this.currentRole = null;
    }
       
    public int getTokens() {
-   
+      return this.rehearseTokens;
    }
    
    public void setTokens(int value) {
-   
+      this.rehearseTokens = value;
+      return;
    }
    
 }
