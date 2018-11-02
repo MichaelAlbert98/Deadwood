41e79c4f223e4724ee290ef542e6ce6d57dce404
albertm4 <albertm4@wwu.edu>
2018-11-02 16:50:57 -0700

    Board.java

--- gameModel/Board.java
+++ gameModel/Board.java
@@ -6,34 +6,41 @@ public class Board {
    private Map<String,Room> nameToRoom;
    private Room[][] adjacentRooms;
    private int scenesRemaining;
-   
+
+   public Board() {
+     this.roomList = null;
+     this.nameToRoom = null;
+     this.adjacentRooms = adjacentRooms;
+     this.scenesRemaining = 0;
+   }
+
    public Board(Room[] roomList, Map<String,Room> nameToRoom, Room[][] adjacentRooms) {
       this.roomList = roomList;
       this.nameToRoom = nameToRoom;
       this.adjacentRooms = adjacentRooms;
       this.scenesRemaining = 10;
    }
-   
+
    public Room nameToRoom(String name) {
       Room room = new Room();
       return room;
    }
-   
+
    public Boolean areRoomsAdjacent(Room start, Room end) {
       return true;
    }
-   
+
    public Boolean isValidMove(Room start, Room end) {
-      return true; 
+      return true;
    }
-   
+
    public Scene getScenesRemaining() {
       Scene scene = new Scene();
       return scene;
    }
-   
+
    public void setScenesRemaining(int value) {
       return;
    }
-   
+
 }
