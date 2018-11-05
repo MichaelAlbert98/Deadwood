1b9ae0e2b7aefd45475a215302be3ccf308f5555
albertm4 <albertm4@wwu.edu>
2018-11-05 13:24:20 -0800

    Fully implemented Room.java

--- gameModel/Room.java
+++ gameModel/Room.java
@@ -7,7 +7,7 @@ public class Room {
    private Player[] playersInRoom;
    private Role[] roomRoles;
    public Scene roomScene;
-   
+
    public Room() {
       this.name = "";
       this.adjacentRooms = null;
@@ -16,7 +16,7 @@ public class Room {
       this.roomRoles = null;
       this.roomScene = null;
    }
-   
+
    public Room(String name, String[] adjRooms, int index, int counters, Role[] roomRoles, Scene roomScene) {
       this.name = name;
       this.adjacentRooms = adjRooms;
@@ -25,54 +25,68 @@ public class Room {
       this.roomRoles = roomRoles;
       this.roomScene = roomScene;
    }
-   
+
    public String getName() {
       return this.name;
    }
-   
+
+   public void setName(String name) {
+     this.name = name;
+     return;
+   }
+
    public String[] getAdjRooms() {
       return this.adjacentRooms;
    }
-   
+
+   public void setAdjRooms(String[] rooms) {
+     this.adjacentRooms = rooms;
+     return;
+   }
+
    public int getRoomIndex() {
       return this.roomIndex;
    }
-   
+
+   public void setRoomIndex(int index) {
+     this.roomIndex = index;
+     return;
+   }
+
    public int getCounters() {
       return this.counters;
    }
-   
+
    public void setCounters(int value) {
       this.counters = value;
       return;
    }
-   
+
    public Player[] getPlayersInRoom() {
       return this.playersInRoom;
    }
-   
+
    public void setPlayersInRoom(Player player) {
       this.playersInRoom[0] = player;
       return;
    }
-   
+
    public Role[] getRoles() {
       return this.roomRoles;
    }
-   
+
    public void setRoles(Role[] roles) {
       this.roomRoles = roles;
-      return;
-   }
-   
-   
-   private void setScene(Scene scene) {
-      this.roomScene = scene;
       return;
    }
 
    public Scene getScene() {
        return this.roomScene;
    }
-   
+
+   public void setScene(Scene scene) {
+      this.roomScene = scene;
+      return;
+   }
+
 }
