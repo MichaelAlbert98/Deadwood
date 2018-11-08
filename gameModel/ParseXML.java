ec7b384b9b119076b7b4612594a4a131ac201b82
albertm4 <albertm4@wwu.edu>
2018-11-07 16:30:28 -0800

    Hopefully made it actually put values into classes.

--- gameModel/ParseXML.java
+++ gameModel/ParseXML.java
@@ -17,15 +17,15 @@ import org.w3c.dom.Element;
 import java.io.File;
 
 public class ParseXML{
-   
+
      // building a document from the XML file
      // returns a Document object after loading the book.xml file.
    public Document getDocFromFile(String filename) throws ParserConfigurationException {
-   
+
       DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
       DocumentBuilder db = dbf.newDocumentBuilder();
       Document doc = null;
-   
+
       try{
          doc = db.parse(filename);
       } catch (Exception ex){
@@ -34,363 +34,349 @@ public class ParseXML{
       }
       return doc;
       // exception handling
-   
+
    }
 
      // reads data from XML file and prints data
-   public void readCardData(Document d){
-   
+   public void readCardData(Document d, Deck deck){
+
       Element root = d.getDocumentElement();
-   
+
       NodeList cards = root.getElementsByTagName("card");
-   
+
       for (int i=0; i<cards.getLength();i++){
-      
-         System.out.println("Printing information for card "+(i+1));
-      
+
              //reads data from the nodes
          Node card = cards.item(i);
+         Scene scene = new Scene();
          String cardName = card.getAttributes().getNamedItem("name").getNodeValue();
-         System.out.println("Name = "+cardName);
+         scene.setName(cardName);
          String cardImg = card.getAttributes().getNamedItem("img").getNodeValue();
-         System.out.println("img = "+cardImg);
+         scene.setImg(cardImg);
          String cardBudget = card.getAttributes().getNamedItem("budget").getNodeValue();
-         System.out.println("budget = "+cardBudget);
-      
+         scene.setBudget(Integer.parseInt(cardBudget));
+
              //reads data
-      
+
          NodeList children = card.getChildNodes();
-      
+
          for (int j=0; j< children.getLength(); j++) {
-         
+
             Node sub = children.item(j);
-         
+
             if("scene".equals(sub.getNodeName())) {
-               System.out.println("Printing information for scene.");
                String sceneNumber = sub.getAttributes().getNamedItem("number").getNodeValue();
-               System.out.println("Number = "+sceneNumber);
+               scene.setSceneNum(Integer.parseInt(sceneNumber));
                String desc = sub.getTextContent();
-               System.out.println("Description = "+desc);
+               scene.setDesc(desc);
             }
-            
+
             else if ("part".equals(sub.getNodeName())) {
-               System.out.println("Part");
+               Role role = new Role();
                String name = sub.getAttributes().getNamedItem("name").getNodeValue();
-               System.out.println("Name = "+name);
+               role.setName(name);
                String level = sub.getAttributes().getNamedItem("level").getNodeValue();
-               System.out.println("Level = "+level);
-               
+               role.setRank(Integer.parseInt(level));
+
                NodeList infos = sub.getChildNodes();
-                 
+
                for (int k=0; k< infos.getLength(); k++) {
-                     
+
                   Node info = infos.item(k);
-                     
+
                   if ("area".equals(info.getNodeName())) {
                      String x = info.getAttributes().getNamedItem("x").getNodeValue();
-                     System.out.println("x = "+x);
+                     role.setxyhw(1,(Integer.parseInt(x)));
                      String y = info.getAttributes().getNamedItem("y").getNodeValue();
-                     System.out.println("y = "+y);
+                     role.setxyhw(2,(Integer.parseInt(y)));
                      String h = info.getAttributes().getNamedItem("h").getNodeValue();
-                     System.out.println("h = "+h);
+                     role.setxyhw(3,(Integer.parseInt(h)));
                      String w = info.getAttributes().getNamedItem("w").getNodeValue();
-                     System.out.println("w = "+w);
+                     role.setxyhw(4,(Integer.parseInt(w)));
                   }
-                  else if ("line".equals(info.getNodeName())) {  
+                  else if ("line".equals(info.getNodeName())) {
                      String line = info.getTextContent();
-                     System.out.println("Line = "+line);
+                     role.setLine(line);
                   }
                }
-            } 
-         
+               scene.setSceneRoles(role);
+            }
+
          } //for. childnodes
-      
-         System.out.println("\n");
-      
+         deck.sceneList.add(scene);
       }//for card nodes
-   
+
    }// method
 
 
    // reads data from XML file and prints data
    public static void readBoardData(Document d){
-   
+
       Element root = d.getDocumentElement();
       NodeList sets = root.getElementsByTagName("set");
       NodeList trailers = root.getElementsByTagName("trailer");
       NodeList offices = root.getElementsByTagName("office");
-      
+
       // reads data from set nodes
       for (int i=0; i<sets.getLength();i++){
-      
-         System.out.println("Printing information for set "+(i+1));
-      
-           //reads data from the nodes
+
+        //reads data from the nodes
+         Room room = new Room();
          Node set = sets.item(i);
          String setName = set.getAttributes().getNamedItem("name").getNodeValue();
-         System.out.println("Name = "+setName);
-      
-           //reads data
-      
+         room.setName(setName);
+
+         //reads data
          NodeList children = set.getChildNodes();
-      
+
          for (int j=0; j< children.getLength(); j++) {
-         
+
             Node sub = children.item(j);
-         
+
             if("neighbors".equals(sub.getNodeName())) {
-               System.out.println("Printing information for neighbors.");
-               
+
                NodeList neighbors = sub.getChildNodes();
-               
+
                for (int k=0; k< neighbors.getLength(); k++) {
-                  
+
                   Node neighbor = neighbors.item(k);
-                  
+
                   if ("neighbor".equals(neighbor.getNodeName())) {
                      String name = neighbor.getAttributes().getNamedItem("name").getNodeValue();
-                     System.out.println("name = "+name);
+                     room.setAdjRooms(name);
                   }
-                  
+
                } //for neighbors nodes
-                       
+
             }
-             
+
             else if ("area".equals(sub.getNodeName())) {
-               System.out.println("Printing information for area.");
                String x = sub.getAttributes().getNamedItem("x").getNodeValue();
-               System.out.println("x = "+x);
+               room.setxyhw(1,Integer.parseInt(x));
                String y = sub.getAttributes().getNamedItem("y").getNodeValue();
-               System.out.println("y = "+y);
+               room.setxyhw(2,(Integer.parseInt(y)));
                String h = sub.getAttributes().getNamedItem("h").getNodeValue();
-               System.out.println("h = "+h);
+               room.setxyhw(3,(Integer.parseInt(h)));
                String w = sub.getAttributes().getNamedItem("w").getNodeValue();
-               System.out.println("w = "+w);
+               room.setxyhw(4,(Integer.parseInt(w)));
             }
-            
+
             else if ("takes".equals(sub.getNodeName())) {
-               System.out.println("Printing information for takes.");
+
                NodeList infos = sub.getChildNodes();
-               
+
                for (int k=0;k< infos.getLength(); k++) {
-                  
+
                   Node info = infos.item(k);
-                  
+
                   if ("take".equals(info.getNodeName())) {
                      String number = info.getAttributes().getNamedItem("number").getNodeValue();
-                     System.out.println("Number = "+number);
-                     
+                     int[] xyhw = new int[4];
                      NodeList areas = info.getChildNodes();
-                     
+
                      for (int l=0;l< areas.getLength(); l++) {
                         Node area = areas.item(l);
                         if ("area".equals(area.getNodeName())) {
                            String x = area.getAttributes().getNamedItem("x").getNodeValue();
-                           System.out.println("x = "+x);
+                           xyhw[1] = Integer.parseInt(x);
                            String y = area.getAttributes().getNamedItem("y").getNodeValue();
-                           System.out.println("y = "+y);
+                           xyhw[2] = Integer.parseInt(y);
                            String h = area.getAttributes().getNamedItem("h").getNodeValue();
-                           System.out.println("h = "+h);
+                           xyhw[3] = Integer.parseInt(h);
                            String w = area.getAttributes().getNamedItem("w").getNodeValue();
-                           System.out.println("w = "+w);
-                        } 
-                        
+                           xyhw[4] = Integer.parseInt(w);
+                           room.setShotsxyhw(xyhw);
+                        }
+
                      }
-                     
+
                   }
-                  
+
                } //for infos
-               
+
             }
-            
+
             else if ("parts".equals(sub.getNodeName())) {
-               System.out.println("Printing information for parts");
-               
+
                NodeList parts = sub.getChildNodes();
-               
+
                for (int k=0;k< parts.getLength(); k++) {
-               
+
                   Node part = parts.item(k);
-                  
+
                   if ("part".equals(part.getNodeName())) {
+                     Role role = new Role();
                      String name = part.getAttributes().getNamedItem("name").getNodeValue();
-                     System.out.println("Name = "+name);
+                     role.setName(name);
                      String level = part.getAttributes().getNamedItem("level").getNodeValue();
-                     System.out.println("Level = "+level);
-                     
+                     role.setRank(Integer.parseInt(level));
+
                      NodeList infos = part.getChildNodes();
-                     
+
                      for (int l=0;l< infos.getLength(); l++) {
-                     
+
                         Node info = infos.item(l);
-                        
+
                         if ("area".equals(info.getNodeName())) {
                            String x = info.getAttributes().getNamedItem("x").getNodeValue();
-                           System.out.println("x = "+x);
+                           role.setxyhw(1,Integer.parseInt(x));
                            String y = info.getAttributes().getNamedItem("y").getNodeValue();
-                           System.out.println("y = "+y);
+                           role.setxyhw(2,Integer.parseInt(y));
                            String h = info.getAttributes().getNamedItem("h").getNodeValue();
-                           System.out.println("h = "+h);
+                           role.setxyhw(3,Integer.parseInt(h));
                            String w = info.getAttributes().getNamedItem("w").getNodeValue();
-                           System.out.println("w = "+w);
-                        } 
+                           role.setxyhw(4,Integer.parseInt(w));
+                        }
                         else if ("line".equals(info.getNodeName())) {
                            String line = info.getTextContent();
-                           System.out.println("Line = "+line);
+                           role.setLine(line);
                         }
-                        
+
                      } //for area & line nodes
-                     
-                  } 
-                  
+
+                  }
+
                } //for part nodes
-               
-            } 
-         
+
+            }
+
          } //for childnodes
-      
-         System.out.println("\n");
-      
+
       }//for set nodes
-      
+
       // reads data from trailer node
       for (int i=0; i<trailers.getLength();i++){
-      
-         System.out.println("Printing information for trailer "+(i+1));
-      
+
          //reads data from the nodes
+         Room room = new Room();
+         room.setName("Trailer");
          Node trailer = trailers.item(i);
-      
+
          NodeList children = trailer.getChildNodes();
-      
+
          for (int j=0; j< children.getLength(); j++) {
-         
+
             Node sub = children.item(j);
-         
+
             if("neighbors".equals(sub.getNodeName())) {
-               System.out.println("Printing information for neighbors.");
-               
+
                NodeList neighbors = sub.getChildNodes();
-               
+
                for (int k=0; k< neighbors.getLength(); k++) {
-                  
+
                   Node neighbor = neighbors.item(k);
-                  
+
                   if ("neighbor".equals(neighbor.getNodeName())) {
                      String name = neighbor.getAttributes().getNamedItem("name").getNodeValue();
-                     System.out.println("name = "+name);
+                     room.setAdjRooms(name);
                   }
-                  
+
                } //for neighbors nodes
-                       
+
             }
-             
+
             else if ("area".equals(sub.getNodeName())) {
-               System.out.println("Printing information for area.");
                String x = sub.getAttributes().getNamedItem("x").getNodeValue();
-               System.out.println("x = "+x);
+               room.setxyhw(1,Integer.parseInt(x));
                String y = sub.getAttributes().getNamedItem("y").getNodeValue();
-               System.out.println("y = "+y);
+               room.setxyhw(2,Integer.parseInt(y));
                String h = sub.getAttributes().getNamedItem("h").getNodeValue();
-               System.out.println("h = "+h);
+               room.setxyhw(3,Integer.parseInt(h));
                String w = sub.getAttributes().getNamedItem("w").getNodeValue();
-               System.out.println("w = "+w);
+               room.setxyhw(4,Integer.parseInt(w));
             }
-         
+
          }
-         
+
       }
-      
+
       // reads data from office node
       for (int i=0; i<offices.getLength();i++){
-      
-         System.out.println("Printing information for office "+(i+1));
-      
+
          //reads data from the nodes
+         Room room = new Room();
+         room.setName("Office");
          Node office = offices.item(i);
-      
+
          NodeList children = office.getChildNodes();
-      
+
          for (int j=0; j< children.getLength(); j++) {
-         
+
             Node sub = children.item(j);
-         
+
             if("neighbors".equals(sub.getNodeName())) {
-               System.out.println("Printing information for neighbors.");
-               
+
                NodeList neighbors = sub.getChildNodes();
-               
+
                for (int k=0; k< neighbors.getLength(); k++) {
-                  
+
                   Node neighbor = neighbors.item(k);
-                  
+
                   if ("neighbor".equals(neighbor.getNodeName())) {
                      String name = neighbor.getAttributes().getNamedItem("name").getNodeValue();
-                     System.out.println("name = "+name);
+                     room.setAdjRooms(name);
                   }
-                  
+
                } //for neighbors nodes
-                       
+
             }
-             
+
             else if ("area".equals(sub.getNodeName())) {
-               System.out.println("Printing information for area.");
                String x = sub.getAttributes().getNamedItem("x").getNodeValue();
-               System.out.println("x = "+x);
+               room.setxyhw(1,Integer.parseInt(x));
                String y = sub.getAttributes().getNamedItem("y").getNodeValue();
-               System.out.println("y = "+y);
+               room.setxyhw(2,Integer.parseInt(y));
                String h = sub.getAttributes().getNamedItem("h").getNodeValue();
-               System.out.println("h = "+h);
+               room.setxyhw(3,Integer.parseInt(h));
                String w = sub.getAttributes().getNamedItem("w").getNodeValue();
-               System.out.println("w = "+w);
+               room.setxyhw(4,Integer.parseInt(w));
             }
-            
+
             else if ("upgrades".equals(sub.getNodeName())) {
                System.out.println("Printing information for upgrades.");
-               
+
                NodeList upgdInfos = sub.getChildNodes();
-               
+
                for (int k=0; k< upgdInfos.getLength(); k++) {
-                  
+
                   Node upgdInfo = upgdInfos.item(k);
-                  
+
                   if ("upgrade".equals(upgdInfo.getNodeName())) {
                      String level = upgdInfo.getAttributes().getNamedItem("level").getNodeValue();
-                     System.out.println("level = "+level);
                      String currency = upgdInfo.getAttributes().getNamedItem("currency").getNodeValue();
-                     System.out.println("currency = "+currency);
                      String amt = upgdInfo.getAttributes().getNamedItem("amt").getNodeValue();
-                     System.out.println("amt = "+amt);
-                     
+
                      NodeList areas = upgdInfo.getChildNodes();
-                     
+                     int[] xyhw = new int[4];
+
                      for (int l=0;l< areas.getLength(); l++) {
-                     
+
                         Node area = areas.item(l);
-                        
+
                         if ("area".equals(area.getNodeName())) {
                            String x = area.getAttributes().getNamedItem("x").getNodeValue();
-                           System.out.println("x = "+x);
+                           xyhw[1] = Integer.parseInt(x);
                            String y = area.getAttributes().getNamedItem("y").getNodeValue();
-                           System.out.println("y = "+y);
+                           xyhw[2] = Integer.parseInt(y);
                            String h = area.getAttributes().getNamedItem("h").getNodeValue();
-                           System.out.println("h = "+h);
+                           xyhw[3] = Integer.parseInt(h);
                            String w = area.getAttributes().getNamedItem("w").getNodeValue();
-                           System.out.println("w = "+w);
-                        } 
-                        
+                           xyhw[4] = Integer.parseInt(w);
+                           room.setShotsxyhw(xyhw);
+                        }
+
                      }
-                     
+
                   }
-                  
+
                }
-               
+
             }
-         
+
          }
-         
+
       }
 
    }// method
-   
+
 }//class
