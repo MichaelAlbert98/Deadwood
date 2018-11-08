006adb99bffcd27c5ebf254103d4e4e4d0e3e09d
albertm4 <albertm4@wwu.edu>
2018-11-07 16:29:58 -0800

    Changed array to arraylist of scenes.

--- gameModel/Deck.java
+++ gameModel/Deck.java
@@ -2,26 +2,26 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Deck {
-   
-   private ArrayList<Scene> sceneList;
+
+   public ArrayList<Scene> sceneList;
    private int topIndex;
-   
+
    public Deck() {
       //TEMPORARY DECK CONSTRUCTOR:
       this.sceneList = new ArrayList<Scene>();
       this.topIndex = 0;
    }
-   
+
    public Deck(ArrayList<Scene> scenes) {
       this.sceneList = scenes;
       this.topIndex = scenes.size()-1;
       shuffleDeck();
    }
-   
+
    private void shuffleDeck() {
       Collections.shuffle(this.sceneList);
    }
-   
+
    public Scene getTopScene() {
       return this.sceneList.get(this.topIndex);
    }
