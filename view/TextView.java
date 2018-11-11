fdae6ea62098dbc01bdc32f3b8e32814d79aa470
rtlingg <linggr@wwu.edu>
2018-11-11 14:49:29 -0800

    Added more game control logic and segmented sections for more organization.

--- view/TextView.java
+++ view/TextView.java
@@ -51,57 +51,33 @@ public class TextView{
      */
     public void startListener() {
         Scanner scanner = new Scanner(System.in);
-        String input;
+        String inputAction;
 
+        startGameMessage();
         this.gameRef.newDay();
         this.gameRef.activePlayer.startPlayerTurn();
         //Switch on player movement
         while (!this.gameRef.isGameOver()) {
 
-            input = scanner.nextLine().toLowerCase();
-            switch (input) {
+            inputAction = scanner.nextLine().toLowerCase();
+            switch (inputAction) {
 
                 //User wants to know current player:
                 case "active player?":
+                case "stats":
                 case "who":
-                    System.out.printf("The active player is %s, your location is %s, you are rank %d, and have $%d and %dcr. "
-                                        ,this.gameRef.activePlayer.getName(),this.gameRef.activePlayer.getLocation().getName(),
-                                        this.gameRef.activePlayer.getRank(), this.gameRef.activePlayer.getCash(), this.gameRef.activePlayer.getCredits());
-                    if (gameRef.activePlayer.getCurrentRole() == null) {
-                        System.out.printf("You are currently not acting.\n");
-                    } else {
-                        System.out.printf("You are currently acting in the scene \"%s\".\n", this.gameRef.activePlayer.getCurrentRole().getName());
-                    }                
+                    activePlayerInfo();                
                     break;
                 case "move":
-                    System.out.println("Which room do you want to move to?");
-                    input = scanner.nextLine().toLowerCase();
-                    //If room exists, check its a valid move, then move the player
-                    if (this.gameRef.board.nameToRoom.get(input) != null){
-                        Room current = this.gameRef.activePlayer.getLocation();
-                        Room dest = this.gameRef.board.nameToRoom.get(input);
-                        if (this.gameRef.board.areRoomsAdjacent(current,dest)) {
-                            this.gameRef.activePlayer.movePlayer(dest);
-                        } else {
-                            System.out.println("You cannot move to that room.");
-                        }
-                    } else {
-                        System.out.println("Room does not exist.");
-                    }
+                    movementPhase(scanner);
                     break;
 
                 case "upgrade":
+                    //upgradePhase(scanner);
                     break;
 
                 case "end turn":
-                    this.gameRef.activePlayer.endPlayerTurn();
-                    this.gameRef.nextPlayer();
-                    if (this.gameRef.isDayOver()) {
-                        this.gameRef.newDay();
-                    }
-                    if (!this.gameRef.isGameOver()) {
-                        this.gameRef.activePlayer.startPlayerTurn();
-                    }
+                    endPlayerTurn();
                     break;
 
                 case "":
@@ -115,4 +91,60 @@ public class TextView{
         scanner.close();
     }
 
+
+    /* Listener Sub-Methods */
+
+    //Active Players Information:
+    private void activePlayerInfo() {
+        System.out.printf("Active player: %s, Location: %s, Rank: %d, Cash: $%d, Credits: %dcr. ",
+                            this.gameRef.activePlayer.getName(),this.gameRef.activePlayer.getLocation().getName(),
+                            this.gameRef.activePlayer.getRank(), this.gameRef.activePlayer.getCash(), this.gameRef.activePlayer.getCredits());
+        if (gameRef.activePlayer.getCurrentRole() == null) {
+            System.out.printf("You are currently not acting.\n");
+        } else {
+            System.out.printf("You are currently acting in the scene \"%s\".\n", this.gameRef.activePlayer.getCurrentRole().getName());
+        }
+    }
+
+    //Movement Helpers:
+    private void movementPhase(Scanner scanner) {
+        System.out.println("Which room do you want to move to?");
+        String input = scanner.nextLine().toLowerCase();
+        //If room exists, check its a valid move, then move the player
+        if (this.gameRef.board.nameToRoom.get(input) != null){
+            Room current = this.gameRef.activePlayer.getLocation();
+            Room dest = this.gameRef.board.nameToRoom.get(input);
+            if (this.gameRef.board.areRoomsAdjacent(current,dest)) {
+                this.gameRef.activePlayer.movePlayer(dest);
+            } else {
+                System.out.println("You cannot move to that room.");
+            }
+        } else {
+            System.out.println("Room does not exist.");
+        }
+    }
+
+    //Upgrade Helpers:
+
+    //End Players Turn:
+    private void endPlayerTurn() {
+        this.gameRef.activePlayer.endPlayerTurn();
+        this.gameRef.nextPlayer();
+        if (this.gameRef.isDayOver()) {
+            this.gameRef.newDay();
+        }
+        if (!this.gameRef.isGameOver()) {
+            this.gameRef.activePlayer.startPlayerTurn();
+        }
+    }
+
+    private void startGameMessage() {
+        System.out.printf("%s%s%s%s%s%s",
+                            "   _____                 _                         _ \n",
+                            "  |  __ \\               | |                       | |\n",
+                            "  | |  | | ___  __ _  __| |_      _____   ___   __| |\n",
+                            "  | |  | |/ _ \\/ _` |/ _` \\ \\ /\\ / / _ \\ / _ \\ / _` |\n",
+                            "  | |__| |  __/ (_| | (_| |\\ V  V / (_) | (_) | (_| |\n",
+                            "  |_____/ \\___|\\__,_|\\__,_| \\_/\\_/ \\___/ \\___/ \\__,_|\n\n");
+    }
 }
