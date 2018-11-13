bf8e9b7d9fdc8135dbb348a575e1777004faba83
MichaelAlbert98 <albertclan@frontier.com>
2018-11-12 20:31:29 -0800

    Added upgrade method.
    Removed Banker.java, added added relevent methods to Player.java

--- gameModel/Player.java
+++ gameModel/Player.java
@@ -55,7 +55,89 @@ public class Player extends Subject {
       return prompt;
    }
 
+   //check if upgrade is valid, if so increase rank and decrease relevent attribute.
+   public void upgrade(String moneyType, int rank) {
+   
+      if (!this.location.getName().equals("Office")) {
+         System.out.println(this.name + " is not at Office. Cannot upgrade.");
+         return;
+      }
+      
+      Boolean canUpgrade = validUpgrade(moneyType,rank) ;
+      
+      if (canUpgrade == false) {
+        return ; }
+      if (moneyType.equals("cash")) {
+         if (rank == 2) {
+            this.rank = 2 ;
+            this.cash = this.cash - 4 ;
+            System.out.println(this.name + " upgraded to rank 2 and lost 4 cash.") ; }
+         else if (rank == 3) {
+            this.rank = 3 ;
+            this.cash = this.cash - 10 ;
+            System.out.println(this.name + " upgraded to rank 3 and lost 10 cash.") ; }
+         else if (rank == 4) {
+            this.rank = 4 ;
+            this.cash = this.cash - 18 ;
+            System.out.println(this.name + " upgraded to rank 4 and lost 18 cash.") ; }
+         else if (rank == 5) {
+            this.rank = 5 ;
+            this.cash = this.cash - 28 ;
+            System.out.println(this.name + " upgraded to rank 5 and lost 28 cash.") ; }
+         else {
+            this.rank = 6 ;
+            this.cash = this.cash - 40 ;
+            System.out.println(this.name + " upgraded to rank 6 and lost 40 cash.") ; }
+      }
 
+         else {
+            if (rank == 2) {
+               this.rank = 2 ;
+               this.credits = this.credits - 5 ;
+               System.out.println(this.name + " upgraded to rank 2 and lost 5 credits.") ; }
+            else if (rank == 3) {
+               this.rank = 3 ;
+               this.credits = this.credits - 10 ;
+               System.out.println(this.name + " upgraded to rank 3 and lost 10 credits.") ; }
+            else if (rank == 4) {
+               this.rank = 4 ;
+               this.credits = this.credits - 15 ;
+               System.out.println(this.name + " upgraded to rank 4 and lost 15 credits.") ; }
+            else if (rank == 5) {
+               this.rank = 5 ;
+               this.credits = this.credits - 20 ;
+               System.out.println(this.name + " upgraded to rank 5 and lost 20 credits.") ; }
+            else {
+               this.rank = 6 ;
+               this.credits = this.credits - 25 ;
+               System.out.println(this.name + " upgraded to rank 6 and lost 25 credits.") ; }
+         return;
+      }
+   }
+
+   //makes sure input is valid and that player can afford upgrade, return false if not.
+   private Boolean validUpgrade(String moneyType, int rank) {
+      if (!moneyType.equals("cash") || !moneyType.equals("credits")) {
+         System.out.println("Input must be cash or credits.") ;
+         return false ; }
+      else if (rank != 2 || rank != 3 || rank != 4  || rank != 5 || rank != 6) {
+         System.out.println("Please input a valid argument for rank.") ;
+         return false ; }
+      else if (moneyType.equals("cash")) {
+         if ((rank == 2 && this.cash < 4) || (rank == 3 && this.cash < 10) || (rank == 4 && this.cash < 18) ||
+            (rank == 5 && this.cash < 28) || (rank == 6 && this.cash < 40)) {
+            System.out.println(this.name + " does not have enough cash to upgrade to this rank.") ;
+            return false ; }
+      }
+      else if (moneyType.equals("credits")) {
+         if ((rank == 2 && this.credits < 5) || (rank == 3 && this.credits < 10) || (rank == 4 && this.credits < 15) ||
+            (rank == 5 && this.credits < 20) || (rank == 6 && this.credits < 25)) {
+            System.out.println(this.name + " does not have enough credits to upgrade to this rank.") ;
+            return false ; }
+      }
+      return true ;
+   }
+   
    /* Getters and Setter for Player */
 
    public String getName() {
