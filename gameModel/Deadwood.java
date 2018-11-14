5c23583ab4e00294f9a22228e8920a9f16f93517
MichaelAlbert98 <albertclan@frontier.com>
2018-11-13 17:53:34 -0800

    Made it so xml files are parsed

--- gameModel/Deadwood.java
+++ gameModel/Deadwood.java
@@ -3,7 +3,8 @@
  * Deadwood.java
  * 11/01/11
  */
-
+ 
+import org.w3c.dom.Document;
 
 public class Deadwood {
 
@@ -36,7 +37,19 @@ public class Deadwood {
             }
 
             //2) Create the game object and the text view model
-            game = new Game(numPlayers,numDays);
+            
+            //read in card data.
+            ParseXML parser = new ParseXML();
+            Document d = parser.getDocFromFile("../figures/cards.xml");
+            Deck deck = new Deck();
+            parser.readCardData(d,deck);
+           
+            //read in board data.
+            d = parser.getDocFromFile("../figures/board.xml");
+            Board board = new Board(deck);
+            parser.readBoardData(d,board);
+            
+            game = new Game(numPlayers,numDays,board);
             TextView tv = new TextView(game);
             System.out.println("Starting Deadwood!\n");
 
