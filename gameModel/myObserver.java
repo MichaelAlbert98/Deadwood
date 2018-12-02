d9fc300456da1fbfa61eff8be97f809df8c00166
MichaelAlbert98 <albertclan@frontier.com>
2018-12-02 15:20:12 -0800

    Changed to interface to allow extending of JFrame.

--- gameModel/myObserver.java
+++ gameModel/myObserver.java
@@ -1,12 +1,12 @@
 /* CSCI 345 - Object Oriented Design - Deadwood
  * Ryan Lingg and Michael Albert
- * myObserver.java - This class is a parent class for classes that need to
+ * myObserver.java - This interface is for classes that need to
  * observe the game.
  * Created 11/02/2018
  * Revised 11/16/2018
  */
 
 //Abstract parent class for the specific observers
-public abstract class myObserver {
-    abstract void update(String message);
+public interface myObserver {
+    void update(String message);
 }
