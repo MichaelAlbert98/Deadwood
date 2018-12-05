bdad680467b7791d05a8e7fc5c3205eefdd6dbe3
albertm4 <albertm4@wwu.edu>
2018-12-05 15:42:57 -0800

    All model change methods implemented.

--- controller/PlayerActionController.java
+++ controller/PlayerActionController.java
@@ -55,18 +55,9 @@ public class PlayerActionController {
     * Displays the valid moves for the player then retireves player input about desired movement.
     * Checks if the movement is valid then updates the players location.
     */
-    public ArrayList<String> movementPhase(String input) {
-        // Prompt section:
-        //displayPlayerPrompt("Which room do you want to move to?", this.gameRef.activePlayer.getLocation().getAdjRooms());
-
-        //FILL IN LAG BUTTONS
-
-        //Try to move player to the new room.
+    public void movementPhase(String input) {
                 this.gameRef.activePlayer.movePlayer(gameRef.board.nameToRoom.get(input));
-                ArrayList<String> actions = determinePlayerActionSet();
-                actions.remove("MOVE");
-                return actions;
-
+                return;
     }
 
     /* Rehearse
@@ -78,60 +69,37 @@ public class PlayerActionController {
         return;
     }
 
-    /* Take Role
+    /* Find Role
      *
-     * Returns true if successfully took role, else false
+     * Returns ArrayList of role names that player can take.
      */
-    public boolean takeRolePhase(){
-        Boolean tookRole = false;
+    public ArrayList<String> findRolePhase() {
         Room location = this.gameRef.activePlayer.getLocation();
-        ArrayList<Role> availableRoles = new ArrayList<Role>();
+        ArrayList<String> availableRoles = new ArrayList<String>();
 
         for (Role role : location.getScene().getSceneRoles()) {
             //If role isnt taken and isnt too high of level, add to list
             if (role.isRoleAvailable(this.gameRef.activePlayer.getRank())) {
-                availableRoles.add(role);
+                availableRoles.add(role.getName());
             }
         }
+        return availableRoles;
+    }
 
-        String availStr = "";
-        if (availableRoles.size() != 0) {
-            int iter = 0;
-            for (Role role : availableRoles) {
-                if (iter != 0) {
-                    availStr = availStr + ", ";
-                }
-                availStr = availStr + "'" + role.getName() + "'";
-                iter++;
+    /* Take Role
+     *
+     * Returns true if successfully took role, else false
+     */
+    public void takeRolePhase(String input){
+        ArrayList<Role> availableRoles = this.gameRef.activePlayer.getLocation().getScene().getSceneRoles();
+        Role roleChosen = null;
+        for (Role role : availableRoles) {
+            if (role.getName().toLowerCase().equals(input.toLowerCase())) {
+                roleChosen = role;
             }
-
-            //DISPLAY ROLES AND GET INPUT: (USE BUTTONS)
-            //System.out.printf("Select a role to take: (options: %s)\n", availStr);
-
-
-            String input = ""; //scanner.nextLine();
-            Role roleChosen = null;
-            for (Role role : availableRoles) {
-                if (role.getName().toLowerCase().equals(input.toLowerCase())) {
-                    roleChosen = role;
-                }
-            }
-
-            if (roleChosen !=  null) {
-                System.out.println("Took role.");
-                this.gameRef.activePlayer.setCurrentRole(roleChosen);
-                roleChosen.setIsTaken(true);
-                tookRole = true;
-            } else {
-                System.out.println("Couldnt find role.");
-            }
-        } else {
-            System.out.println("No roles available for your rank. Come back with more experiance.\n");
-            tookRole = true;
         }
-
-
-        return tookRole;
+        this.gameRef.activePlayer.setCurrentRole(roleChosen);
+        roleChosen.setIsTaken(true);
     }
 
     /* Acting Phase
@@ -176,47 +144,80 @@ public class PlayerActionController {
      * Retireves player input about desired upgrade.
      * Then calls upgrade with data to check if upgrade is valid and performs upgrade.
      */
-    public Boolean upgradePhase() {
-        String input;
-        int desiredRank;
-        String desiredPaymentType;
-
-        // Check Location
-        if (this.gameRef.activePlayer.getLocation().getName().equals("Office")) {
-            // Check Rank
-            if (this.gameRef.activePlayer.getRank() == 6) {
-                System.out.println("You are already the max actor rank.");
-            } else {
-                System.out.print("Select the desired rank you wish to upgrade too. The upgrades and costs are the following: \n" +
-                        "Rank: \t Cash \t Credit \n 2 \t 4 \t 5 \n 3 \t 10 \t 10 \n 4 \t 18 \t 15 \n 5 \t 28 \t 20" +
-                        "\n 6 \t 40 \t 25 \n Select the desired rank: ");
-                input = ""; //scanner.nextLine().toLowerCase();
-                // Check Input
-                try {
-                    desiredRank = Integer.parseInt(input);
-                    if ((desiredRank <= this.gameRef.activePlayer.getRank())) {
-                        System.out.println("Invlid rank. You must choose a rank higher than your current rank.");
-                    } else if (desiredRank > 6) {
-                        System.out.println("Invlaid rank. You cannot choose a acting rank higher than the max of 6.");
-                    } else {
-                        System.out.print("Select the desired payment type ('Cash' or 'Credit'): ");
-                        input = ""; //scanner.nextLine();
-                        desiredPaymentType = input.toLowerCase();
-                        if (desiredPaymentType.equals("cash") || desiredPaymentType.equals("credit")) {
-                            return upgrade(desiredPaymentType, desiredRank);
-                        } else {
-                            System.out.println("Invalid payment type selected.");
-                        }
-                    }
-
-                } catch (Exception e) {
-                    System.out.println("Desired rank must only contain a integer.");
-                }
+    public ArrayList<Integer> getLevels(String type) {
+        ArrayList<Integer> levels = new ArrayList<Integer>();
+        if (type.equals("Cash")) {
+            int cash = gameRef.activePlayer.getCash();
+            int rank = gameRef.activePlayer.getRank();
+            if (cash >= 4 && rank == 1) {
+                levels.add(2);
             }
-        } else {
-            System.out.println("You must be in the casting office to upgrade your actor rank.");
+            if (cash >= 10 && rank <= 2) {
+                levels.add(3);
+            }
+            if (cash >= 18 && rank <= 3) {
+                levels.add(4);
+            }
+            if (cash >= 28 && rank <= 4) {
+                levels.add(5);
+            }
+            if (cash >= 40 && rank <= 5) {
+                levels.add(6);
+            }
         }
-        return false;
+        if (type.equals("Credits")) {
+            int credits = gameRef.activePlayer.getCredits();
+            int rank = gameRef.activePlayer.getRank();
+            if (credits >= 5 && rank == 1) {
+                levels.add(2);
+            }
+            if (credits >= 10 && rank <= 2) {
+                levels.add(3);
+            }
+            if (credits >= 15 && rank <= 3) {
+                levels.add(4);
+            }
+            if (credits >= 20 && rank <= 4) {
+                levels.add(5);
+            }
+            if (credits >= 25 && rank <= 5) {
+                levels.add(6);
+            }
+        }
+        return levels;
+    }
+
+    /* Upgrade Phase
+     *
+     * Retireves player input about desired upgrade.
+     * Then calls upgrade with data to check if upgrade is valid and performs upgrade.
+     */
+    public ArrayList<String> getMoneyTypes() {
+        ArrayList<String> types = new ArrayList<String>();
+        if (gameRef.activePlayer.getCash() >= 4)
+            types.add("Cash");
+        if (gameRef.activePlayer.getCredits() >= 5) {
+            types.add("Credits");
+        }
+        return types;
+    }
+
+    /* Upgrade Phase
+     *
+     * Retireves player input about desired upgrade.
+     * Then calls upgrade with data to check if upgrade is valid and performs upgrade.
+     */
+    public void upgradePhase(String moneyType,int level) {
+        int[] cashLevels = {4,10,18,28,40};
+        int[] creditLevels = {5,10,15,20,25};
+        gameRef.activePlayer.setRank(level);
+        if (moneyType.equals("Cash")) {
+            gameRef.activePlayer.removeCurrencies(cashLevels[level-2],0);
+        }
+        if (moneyType.equals("Credits")) {
+            gameRef.activePlayer.removeCurrencies(0,creditLevels[level-2]);
+        }
+        return;
     }
 
 
@@ -225,7 +226,7 @@ public class PlayerActionController {
      * Once a valid rank and money type have been input is entered this method checks if upgrade is
      * valid, and  upgrades the player if so, else notitifies the player of invalid choice
      */
-    public boolean upgrade(String moneyType, int rank) {
+  /*  public boolean upgrade(String moneyType, int rank) {
         Boolean canUpgrade = validUpgrade(moneyType, rank);
         if (canUpgrade == false) {
             return false;
@@ -279,14 +280,14 @@ public class PlayerActionController {
             }
         }
         return true;
-    }
+    } */
 
 
     /* Valid Upgrade Checker
      *
      * Makes sure input is valid and that player can afford upgrade, return false if not
      */
-    private Boolean validUpgrade(String moneyType, int rank) {
+  /*  private Boolean validUpgrade(String moneyType, int rank) {
         if (moneyType.equals("cash")) {
             if ((rank == 2 && this.gameRef.activePlayer.getCash() < 4) || (rank == 3 && this.gameRef.activePlayer.getCash() < 10) || (rank == 4 && this.gameRef.activePlayer.getCash() < 18)
                     || (rank == 5 && this.gameRef.activePlayer.getCash() < 28) || (rank == 6 && this.gameRef.activePlayer.getCash() < 40)) {
@@ -301,7 +302,7 @@ public class PlayerActionController {
             }
         }
         return true;
-    }
+    } */
 
     /* End Player Turn
      *
